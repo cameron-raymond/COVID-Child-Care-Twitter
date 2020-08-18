b2c63a298d4b1b0465127b4b031c1639a898da67
Cameron Raymond <cameron.raymond@hey.com>
2020-08-18 11:16:22 -0400

    working on exploratory analysis

---
+++ notebooks/utils.py
@@ -0,0 +1,36 @@
+import ast
+
+DTYPE = {
+    "id": "str",
+    "created_at": "str",
+    "screen_name": "str",
+    "source": "str",
+    "clean_text": "str",
+    "original_text": "str",
+    "is_retweet": "bool",
+    "favorite_count": "int",
+    "retweet_count": "int",
+    "hashtags": "str",
+    "urls": "str",
+    "mentions": "str",
+    "city": "str",
+    "province": "str",
+    "longitude": "float",
+    "latitude": "float",
+}
+
+PROVINCES = ["Alberta",
+            "British Columbia",
+            "Manitoba",
+            "New Brunswick",
+            "Newfoundland and Labrador",
+            "Northwest Territories",
+            "Nova Scotia",
+            "Nunavut",
+            "Ontario",
+            "Prince Edward Island",
+            "Quebec",
+            "Saskatchewan",
+            "Yukon"]
+
+PARSE_DATES = ["created_at"]
